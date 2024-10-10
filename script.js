96beea236117deaa90d404c47cb3974e87fb135e
Kawatavares22A A <kawa556622@gmail.com>
2024-10-10 01:25:31 +0000

    trabalho

---
+++ script.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')();
+
+let alcool = 0
+let gasolina = 0
+let diesel = 0
+let resposta = 0
+
+while (resposta !== 4) {
+    resposta = Number(input("Escolha seu combustível (1 - Álcool, 2 - Gasolina e 3 - Diesel"))
+   if (resposta === 1) {
+      alcool++
+} ElseIf (resposta === 2) {
+      gasolina++
+} Else  (resposta === 3) {
+      diesel++
+}
+}
+
+console.log("muito obrigado")
+console.log(` Álcool: ${alcool} `)
+console.log(` Gasolina: ${gasolina} `)
+console.log(` Diesel: ${diesel} `)
